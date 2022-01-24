e0c88c638b0e8695fe097028b0564a311446b294
haroldocruz <msom.info@gmail.com>
2022-01-24 18:08:37 -0300

    app: updated json and urlencoded; package body-parser deprecated

--- src/app.js
+++ src/app.js
@@ -3,11 +3,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-// require('typescript-require')
-
-// var multer = require('multer')
 
 var app = express();
 
@@ -18,10 +14,12 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static('public'));
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')))
 
 const headers = require('./appHeaders');
 
@@ -36,7 +34,10 @@ app.use(function (req, res, next) {
 // middleware that is specific to this router
 app.use(function timeLog(req, res, next) {
 
-  console.log("MIDDLEWARE: " + JSON.stringify(req.body)) //! APAGAR
+  console.log("MIDDLEWARE-headers: " + JSON.stringify(req.headers)) //! APAGAR
+  console.log("MIDDLEWARE-body: " + JSON.stringify(req.body)) //! APAGAR
+  console.log("MIDDLEWARE-file: " + JSON.stringify(req.file)) //! APAGAR
+  console.log("MIDDLEWARE-files: " + JSON.stringify(req.files)) //! APAGAR
 
   console.log('\nTime: ', new Date);
   next();
@@ -58,6 +59,7 @@ app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  console.log("res.locals.error", res.locals.error);
 
   // render the error page
   res.status(err.status || 500);
